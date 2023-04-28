11e3927a7355b01161855a74ab280e732ca0d36f
WilliamPascal <williampascal_official@outlook.com>
2023-04-28 08:26:42 +0800

    (ADD) iohandle.h/.c
    Added iohandle.h/.c to be able to receive input.

--- src/iohandle.h
+++ src/iohandle.h
@@ -6,19 +6,40 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <uchar.h>
 
-uint16 BUFFMAX = 4068;
-uint16 WBUFFMAX = (BUFFMAX * 2);
+uint16_t BUFFMAX = 4068;
+uint16_t WBUFFMAX = (BUFFMAX * 2);
+
+uint16_t REDIRMAX = 512;
 
 char buffer[BUFFMAX] = {};
-wchar_t wbuffer[WBUFFMAX] = {};
+char16_t wbuffer[WBUFFMAX] = {};
 
-/* YOU LEFT HERE */
-
-typedef struct _input_handle_S
+typedef enum _encoding_type_E
 {
+  /* Unit is "Byte" */
+  NONE   = 0,  /* 0 Bit   */
+  ASCII  = 1,  /* 8 Bits  */
+  UTF_8  = 1,  /* 8 Bits  */
+  UTF_16 = 2,  /* 16 Bits */
+  UTF_32 = 4,  /* 32 Bits */
+# ifdef __WIN32
+  ALL    = 4   /* 32 Bits */
+# elif defined __WIN64  /* __WIN32 */
+  ALL    = 8   /* 64 Bits */
+# else  /* __WIN64 */
+# error Unsupported platform
+#endif  /* ELSE */
+}
+
+typedef struct _IO_handle_S
+{
+  /* For inputting from multi-channels */
+  FILE* _redirection[REDIRMAX];
   
-} input_handle_t;
+  
+} IO_handle_t;
 
 #endif /* NO IOHANDLE_H */
 
